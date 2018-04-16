6906d7d6ded714a1b4b0f1831b75bb543fdd3c85
Georgia Hardy <ghardy1@sheffield.ac.uk>
2018-04-16 11:45:41 +0100

    getting rid of stuff I don't need

--- controllers/characters.js
+++
@@ -1,7 +0,0 @@
-var Character = new Schema(
-    {
-        name: {type: String, required: true, max: 100},
-        cuisine: {type: String, },
-        postcode: {type: String},
-        address: {type: String}
-    } );

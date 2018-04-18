f3b4528a142d4f82e49e64960a80670721f03127
Georgia Hardy <ghardy1@sheffield.ac.uk>
2018-04-18 12:41:11 +0100

    controller still not fully working

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -2,12 +2,19 @@ const models = require('../models/restaurant');
 
 // noinspection JSAnnotator
 module.exports = {
+
     findRest: function(req, res, next)  {
         models.Restaurant.find(function (err, results)  {
             if (err) {
                 return console.error(err);
             }
             console.log(results);
+            if(results !== null)  {
+                res.render(results);
+            }
+            else  {
+                res.render('home', {foundRest: false, error: "cannot find details"});
+            }
         });
     }
 };
